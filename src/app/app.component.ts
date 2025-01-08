04ed15289979f4d6a2359110261c6339d5ef0421
Oindrila Ghosh (OSV) <oindrila.ghosh.osv@fedex.com>
2025-01-08 14:22:56 +0530

    fedex webapp sub menu changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BusinessfedexComponent } from './businessfedex/businessfedex.component';
-import { PeoplefedexComponent } from './peoplefedex/peoplefedex.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [HeaderComponent,
-    FooterComponent,RouterOutlet,RouterLink,RouterLinkActive],
+    FooterComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'fedex_webapp';
+  showSubMenu: boolean = false;
+
+  // toggleSubMenu(isVisible: boolean): void {
+  //   console.log("isvisible-", isVisible)
+  //   this.showSubMenu = true;
+  //   this.cdr.detectChanges();
+  // }
+
+  // @HostListener('window:click', ['$event']) 
+  // onWindowClick(event: MouseEvent): void {
+  //   this.showSubMenu = false;
+   
+  // }
+
 }
