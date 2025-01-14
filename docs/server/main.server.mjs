d8fb1ab82ba57c087e2cad5ef17bc5487e868064
Bhalla <jitender.bhalla@accenture.com>
2025-01-14 18:23:04 +0530

    add docs

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NCNT72AN.mjs";import"./chunk-DK5O3YHJ.mjs";import"./chunk-7D24ICYA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-QKHNZLZL.mjs";import"./chunk-DK5O3YHJ.mjs";import"./chunk-7D24ICYA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
